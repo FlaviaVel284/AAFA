dd3917e06da6b305dd02c8030814110268eb9609
Flavia Velcsov <v_flavia99@yahoo.com>
2020-06-19 17:49:10 +0300

    Editing Button optimized

--- src/main/java/aafa/Client/EditAnnouncementForm.java
+++ src/main/java/aafa/Client/EditAnnouncementForm.java
@@ -2,9 +2,11 @@ package aafa.Client;
 
 import aafa.data.Announcement;
 import aafa.exceptions.CouldNotWriteAnnouncemetException;
+import aafa.exceptions.EnterOwnerName;
 import aafa.services.AnnouncementService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -25,29 +27,43 @@ public class EditAnnouncementForm {
     public TextField picturePathField;
 
     public void postButton(ActionEvent event) throws CouldNotWriteAnnouncemetException {
+        Announcement ann;
+       try {
 
-        Announcement ann=AnnouncementService.getAnnouncement(ownerField.getText());
-        if(ownerField!=null) {
-            ann.setOwner(ownerField.getText());
-        }
-        if(ownerField!=null) {
-            ann.setSpecies(speciesField.getText());
-        }
-        if(ownerField!=null) {
-            ann.setAge(ageField.getText());
-        }
-        if(ownerField!=null) {
-            ann.setMedicalStatus(medicalStatusField.getText());
-        }
-        if(ownerField!=null) {
-            ann.setAddress(addressField.getText());
-        }
-        if(ownerField!=null) {
-        ann.setPathPhoto(picturePathField.getText());
-        }
-        AnnouncementService.persistAnnouncement();
+           if (!ownerField.getText().equals("")) {
+               ann=AnnouncementService.getAnnouncement(ownerField.getText());
+               ann.setOwner(ownerField.getText());
+           }else
+           {
+               throw new EnterOwnerName();
+           }
 
-        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+           if (!speciesField.getText().equals("")) {
+               ann.setSpecies(speciesField.getText());
+           }
+           if (!ageField.getText().equals("")) {
+               ann.setAge(ageField.getText());
+           }
+           if (!medicalStatusField.getText().equals("")) {
+               ann.setMedicalStatus(medicalStatusField.getText());
+           }
+           if (!addressField.getText().equals("")) {
+               ann.setAddress(addressField.getText());
+           }
+           if (!picturePathField.getText().equals("")) {
+               ann.setPathPhoto(picturePathField.getText());
+           }
+
+           AnnouncementService.persistAnnouncement();
+           ((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
+       }catch(EnterOwnerName e){
+           Alert alert = new Alert(Alert.AlertType.INFORMATION);
+           alert.setTitle("Please enter owner name!");
+           alert.setHeaderText("Please enter owner name!");
+           alert.setContentText("Please enter owner name!");
+           System.out.println();
+           alert.showAndWait();
+       }
     }
 
 }
