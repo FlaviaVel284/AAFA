450ed011510bd0b152d971e68ff324d19a303f75
VanciaG <vancia.gabriela@yahoo.com>
2020-06-20 17:34:23 +0300

    #AAFA-74 Created GUI form for List Of Clients

--- src/main/java/aafa/controllers/ShelterMainScreen.java
+++ src/main/java/aafa/controllers/ShelterMainScreen.java
@@ -55,6 +55,25 @@ public class ShelterMainScreen {
         window.show();
     }
 
+    public void ListOfClientsButtonPushed(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getClassLoader().getResource("ListOfClients.fxml"));
+        AnchorPane page = loader.load();
+
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("List Of Clients");
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        dialogStage.initOwner(((Node) event.getSource()).getScene().getWindow());
+        Scene scene = new Scene(page);
+        dialogStage.setScene(scene);
+
+        dialogStage.showAndWait();
+
+
+
+
+    }
+
 
 
 }
